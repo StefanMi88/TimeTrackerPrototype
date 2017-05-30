e8332bc807d7ac5f0705019293e101236b09f880
dark1088 <dark88@gmx.at>
2017-05-30 23:19:19 +0200

    mit Insert Befehlen versehen
    Signed-off-by: dark1088 <dark88@gmx.at>

--- Datenbank/category.sql
+++ Datenbank/category.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 28. Apr 2017 um 10:49
+-- Erstellungszeit: 30. Mai 2017 um 23:14
 -- Server-Version: 10.1.21-MariaDB
--- PHP-Version: 7.1.1
+-- PHP-Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,6 +32,21 @@ CREATE TABLE `category` (
   `description` varchar(200) NOT NULL,
   `project_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `description`, `project_id`) VALUES
+(1, 'Organisation', 'alles Organisatorisches (Projekt-Management)', 1),
+(2, 'Analyse', 'Analyse von neue Aufgaben / Features; Aufwandschätzung', 1),
+(3, 'Implementierung', 'Umsetzung der Aufgabe / Features', 1),
+(4, 'Testen', 'Test der Aufgabe / Features', 1),
+(5, 'Dokumentieren', 'Dokumenterien der Aufgabe / Features / Programme', 1),
+(6, 'Bugfix', 'Fehlerbehebung der Aufgabe / Features', 1),
+(7, 'Support', 'Support für Aufgabe / Features', 1),
+(8, 'Schulung/Workshop', 'Schulen der Aufgabe / Features / Programme', 1),
+(9, 'Weiterbildung', 'Weiterbildung für die Umsetzung der Aufgabe / Features', 1);
 
 --
 -- Indizes der exportierten Tabellen
