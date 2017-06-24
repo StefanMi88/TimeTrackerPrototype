f8f741886be539521432a8ad0665cddfd7f1ae9a
StefanMit <stefan.mitmansgruber@gmail.com>
2017-06-24 14:23:45 +0200

    table projectmembers added

--- Datenbank/timetracker.sql
+++ Datenbank/timetracker.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 20. Jun 2017 um 09:28
+-- Erstellungszeit: 24. Jun 2017 um 14:22
 -- Server-Version: 10.1.21-MariaDB
--- PHP-Version: 7.1.1
+-- PHP-Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Tabellenstruktur für Tabelle `category`
 --
 
-DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -47,7 +46,6 @@ INSERT INTO `category` (`id`, `name`, `description`, `project_id`) VALUES
 -- Tabellenstruktur für Tabelle `project`
 --
 
-DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -60,7 +58,33 @@ CREATE TABLE `project` (
 
 INSERT INTO `project` (`id`, `name`, `description`) VALUES
 (2, 'projecta', 'vdvdfvcvcv'),
-(3, 'test', 'max65');
+(3, 'test', 'max65'),
+(4, 'Test Project', 'Test from Stefan');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `projectmembers`
+--
+
+CREATE TABLE `projectmembers` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) COLLATE utf8_bin NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Daten für Tabelle `projectmembers`
+--
+
+INSERT INTO `projectmembers` (`id`, `username`, `project_id`, `admin`) VALUES
+(34, 'max1', 3, 0),
+(35, 'admin', 3, 0),
+(36, 'max', 3, 0),
+(37, 'admin2', 3, 0),
+(38, 'oliver', 3, 0),
+(39, 'admin2', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -68,7 +92,6 @@ INSERT INTO `project` (`id`, `name`, `description`) VALUES
 -- Tabellenstruktur für Tabelle `task`
 --
 
-DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -81,17 +104,14 @@ CREATE TABLE `task` (
 --
 
 INSERT INTO `task` (`id`, `name`, `description`, `project_id`) VALUES
-(2, 'dfd', 'fdf', 2),
-(3, 'fdgfdgfdgf', 'gfg', 2),
-(4, 'fdfd', 'tere', 2),
-(5, 'vvc', 'vcv', 2),
-(6, 'fdfd', 'gfgf', 3),
-(7, 'fdsdfd', 'trtrt', 2),
-(8, 'msad', 'max', 2),
-(9, 'oo', 'oo', 2),
-(10, 'ghghg', 'oujik', 2),
-(11, 'hghg', 'gfgf', 3),
-(12, 'gfdg', 'bgfh', 2);
+(13, 'test', 'test', 4),
+(15, 'test2', 'test2', 4),
+(16, 'aetaet', 'aetaet', 4),
+(17, 'teatewtat', 'test3235', 4),
+(18, 'afe', 'aef', 4),
+(19, 'afef', 'afef', 4),
+(20, 'aefa', 'aef', 4),
+(21, 'test', 'test', 3);
 
 -- --------------------------------------------------------
 
@@ -99,7 +119,6 @@ INSERT INTO `task` (`id`, `name`, `description`, `project_id`) VALUES
 -- Tabellenstruktur für Tabelle `time`
 --
 
-DROP TABLE IF EXISTS `time`;
 CREATE TABLE `time` (
   `task_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -113,10 +132,14 @@ CREATE TABLE `time` (
 
 INSERT INTO `time` (`task_id`, `user_id`, `start`, `end`) VALUES
 (3, 1, '2017-06-19 14:16:28', '2017-06-19 14:16:34'),
+(5, 1, '2017-06-21 16:11:34', '2017-06-21 16:13:09'),
 (6, 1, '2017-06-19 14:15:31', '2017-06-19 14:16:39'),
 (8, 1, '2017-06-19 14:11:43', '2017-06-19 14:15:26'),
 (12, 1, '2017-06-19 14:10:58', '2017-06-19 14:11:26'),
-(12, 1, '2017-06-19 14:16:42', '2017-06-19 14:16:46');
+(12, 1, '2017-06-19 14:16:42', '2017-06-19 14:16:46'),
+(13, 1, '2017-06-21 16:18:32', '2017-06-21 16:18:34'),
+(13, 1, '2017-06-21 16:18:37', '2017-06-21 16:18:40'),
+(18, 1, '2017-06-24 12:19:22', '2017-06-24 12:19:29');
 
 -- --------------------------------------------------------
 
@@ -124,8 +147,8 @@ INSERT INTO `time` (`task_id`, `user_id`, `start`, `end`) VALUES
 -- Tabellenstruktur für Tabelle `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `type` varchar(20) NOT NULL,
@@ -144,12 +167,12 @@ CREATE TABLE `user` (
 -- Daten für Tabelle `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `type`, `company`, `email`, `firstname`, `lastname`, `address`, `city`, `country`, `zip`, `aboutme`) VALUES
-('max1', 'admin', 'admin', '', '', '', '', '', '', '', '', ''),
-('admin', 'admin', 'admin', '', '', '', '', '', '', '', '', ''),
-('max', 'max', 'user', 'jks', 'jkldj@gmx.at', 'jj', 'jij', 'joij', 'jio', 'jlkj', '', 'jiojjjjoijoji'),
-('admin2', 'admin', 'USER', 'ds', 'sd@gmal.at', 'dsd', 'sds', 'dsds', 'vdv', 'dfd', 'fdfd', '\'\'\'\'dffdf'),
-('oliver', 'oliver', 'ADMIN', 'jku', 'oo@gmx.at', 'oliver', 'obern', 'natternbach', 'linz', 'at', '4040', 'wappla');
+INSERT INTO `user` (`id`, `username`, `password`, `type`, `company`, `email`, `firstname`, `lastname`, `address`, `city`, `country`, `zip`, `aboutme`) VALUES
+(1, 'max1', 'admin', 'admin', '', '', '', '', '', '', '', '', ''),
+(2, 'admin', 'admin', 'admin', '', '', '', '', '', '', '', '', ''),
+(3, 'max', 'max', 'user', 'jks', 'jkldj@gmx.at', 'jj', 'jij', 'joij', 'jio', 'jlkj', '', 'jiojjjjoijoji'),
+(4, 'admin2', 'admin', 'USER', 'ds', 'sd@gmal.at', 'dsd', 'sds', 'dsds', 'vdv', 'dfd', 'fdfd', '\'\'\'\'dffdf'),
+(5, 'oliver', 'oliver', 'ADMIN', 'jku', 'oo@gmx.at', 'oliver', 'obern', 'natternbach', 'linz', 'at', '4040', 'wappla');
 
 --
 -- Indizes der exportierten Tabellen
@@ -168,6 +191,12 @@ ALTER TABLE `project`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indizes für die Tabelle `projectmembers`
+--
+ALTER TABLE `projectmembers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indizes für die Tabelle `task`
 --
 ALTER TABLE `task`
@@ -179,6 +208,26 @@ ALTER TABLE `task`
 ALTER TABLE `time`
   ADD PRIMARY KEY (`task_id`,`user_id`,`start`);
 
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `projectmembers`
+--
+ALTER TABLE `projectmembers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+--
+-- AUTO_INCREMENT für Tabelle `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
