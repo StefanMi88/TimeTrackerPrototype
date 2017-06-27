f6ef32e9bb63eaf58a02dca902f224d993a0add6
StefanMit <stefan.mitmansgruber@gmail.com>
2017-06-27 17:34:03 +0200

    Projectmembers.class

---
+++ timetracker-app/src/main/java/at/jku/timetracker/model/Projectmembers.java
@@ -0,0 +1,57 @@
+package at.jku.timetracker.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="PROJECT")
+public class Projectmembers {
+
+	@Id
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name = "USERNAME")
+	private String username;
+	
+	@Column(name = "PROJECT_ID")
+	private int project_id;
+
+	
+	public Projectmembers() {
+		super();
+	}
+
+	public Projectmembers(int id, String username, int project_id) {
+		this.id = id;
+		this.username = username;
+		this.project_id = project_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getProjectId() {
+		return project_id;
+	}
+
+	public void setProjectId(int project_id) {
+		this.project_id = project_id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
